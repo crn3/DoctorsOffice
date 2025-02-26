9e833e8a866e6afb2616022d0a735e64f39dee7c
Rachael Kenny <157695390+crn3@users.noreply.github.com>
2025-02-26 15:44:52 +0000

    appointment index not working

--- MyProj.DataAccess/obj/Debug/net8.0/MyProj.DataAccess.AssemblyInfo.cs
+++ MyProj.DataAccess/obj/Debug/net8.0/MyProj.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProj.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d59040e90aac2bc7edf6d757f2e5088ed35d2a2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProj.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProj.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Services/obj/Debug/net8.0/MyProj.Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/MyProj.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProj.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d59040e90aac2bc7edf6d757f2e5088ed35d2a2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProj.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProj.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
